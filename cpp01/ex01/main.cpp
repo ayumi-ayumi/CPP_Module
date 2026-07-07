8c7ac30d080cc808cb97c6e5051e0fe6b3896927
Ayumi <ayuayuayuayus@gmail.com>
2026-07-07 20:32:33 +0200

    CHANGE program name

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -3,7 +3,10 @@
 int main()
 {
      Zombie* horde = zombieHorde(5, "Wood");
+     if (!horde)
+      return (1);
      for (int i = 0; i < 5; i++)
         horde[i].announce();
      delete[] horde;
+     return (0);
 }
