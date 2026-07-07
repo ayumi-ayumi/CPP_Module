1bad264d2949effcc9b738e31326961bf858a8b8
Ayumi <ayuayuayuayus@gmail.com>
2026-07-07 23:27:39 +0200

    RENAME name => m_name

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -3,18 +3,18 @@
 #include <string>
 
 // Constructor. initialize m_name as name
-Zombie::Zombie(const std::string name) : m_name(name)
+Zombie::Zombie(const std::string name) : name(name)
 {
-	std::cout <<  m_name << ": Zombie created" << std::endl;
+	std::cout <<  name << ": Zombie created" << std::endl;
 };
 
 // Destructor
 Zombie::~Zombie()
 {
-	std::cout << m_name << ": Zombie destroyed" << std::endl;
+	std::cout << name << ": Zombie destroyed" << std::endl;
 };
 
 void	Zombie::announce(void)
 {
-	std::cout << m_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 };
--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -9,7 +9,7 @@ class Zombie
 		~Zombie();
 		void announce(void);
 	private:
-		std::string m_name;
+		std::string name;
 };
 
 Zombie* newZombie( std::string name );
