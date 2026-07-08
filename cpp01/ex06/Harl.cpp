3ef78beed3184982837f25d36e04144c4a5ab5cd
Ayumi <ayuayuayuayus@gmail.com>
2026-07-08 19:54:29 +0200

    DONE ex06

---
+++ cpp01/ex06/Harl.cpp
@@ -0,0 +1,72 @@
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+    fs[0] = &Harl::debug;
+    fs[1] = &Harl::info;
+    fs[2] = &Harl::warning;
+    fs[3] = &Harl::error;
+};
+
+Harl::~Harl() {};
+
+enum levelType
+{
+    debug = 0,
+    info,
+    warning,
+    error,
+    unknown,
+};
+
+levelType getlevelType(const std::string level)
+{
+    if (level == "DEBUG")
+        return debug;
+    if (level == "INFO")
+        return info;
+    if (level == "WARNING")
+        return warning;
+    if (level == "ERROR")
+        return error;
+    return unknown;
+};
+
+void Harl::complain(std::string level)
+{
+    int levelType = getlevelType(level);
+    switch (levelType)
+    {
+        case 0:
+            (this->*fs[0])();
+        case 1:
+            (this->*fs[1])();
+        case 2:
+            (this->*fs[2])();
+        case 3:
+            (this->*fs[3])();
+            break;
+        default:
+            std::cout << "|--- Invalid input ---|" << std::endl;
+    }
+};
+
+void Harl::debug(void)
+{
+    std::cout << "<<DEBUG>> I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+    std::cout << std::endl;
+};
+void Harl::info(void)
+{
+    std::cout << "<<INFO>> I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << std::endl;
+};
+void Harl::warning(void)
+{
+    std::cout << "<<WARNING>> I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+    std::cout << std::endl;
+};
+void Harl::error(void)
+{
+    std::cout << "<<ERROR>> This is unacceptable! I want to speak to the manager now." << std::endl;
+};
---
+++ cpp01/ex06/Harl.hpp
@@ -0,0 +1,19 @@
+#ifndef _HARL_H_
+#define _HARL_H_
+#include <string>
+#include <iostream>
+
+class Harl
+{
+    public:
+        Harl();
+        ~Harl();
+        void complain( std::string level );
+    private:
+        void(Harl::*fs[4])(void);
+        void debug( void );
+        void info( void );
+        void warning( void );
+        void error( void );
+};
+#endif
