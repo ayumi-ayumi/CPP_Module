8f580d09726d0f18cf9c4cbe5d2c3c0981e85319
Ayumi <ayuayuayuayus@gmail.com>
2026-07-08 19:54:56 +0200

    CHANGE return message

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -8,9 +8,9 @@ Harl::Harl()
     fs[3] = &Harl::error;
 };
 
-Harl::~Harl(){};
+Harl::~Harl() {};
 
-enum levelType 
+enum levelType
 {
     debug = 0,
     info,
@@ -19,11 +19,16 @@ enum levelType
     unknown,
 };
 
-levelType getlevelType(const std::string level) {
-    if (level == "DEBUG") return debug;
-    if (level == "INFO")   return info;
-    if (level == "WARNING")  return warning;
-    if (level == "ERROR") return error;
+levelType getlevelType(const std::string level)
+{
+    if (level == "DEBUG")
+        return debug;
+    if (level == "INFO")
+        return info;
+    if (level == "WARNING")
+        return warning;
+    if (level == "ERROR")
+        return error;
     return unknown;
 };
 
@@ -33,24 +38,24 @@ void Harl::complain(std::string level)
     if (levelType >= 4)
     {
         std::cout << "|--- Invalid input ---|" << std::endl;
-        return ;
+        return;
     }
     (this->*fs[levelType])();
 };
 
 void Harl::debug(void)
 {
-    std::cout << "debug: I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+    std::cout << "<<DEBUG>> I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 };
 void Harl::info(void)
 {
-    std::cout << "info: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << "<<INFO>> I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 };
 void Harl::warning(void)
 {
-    std::cout << "warning: I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+    std::cout << "<<WARNING>> I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
 };
 void Harl::error(void)
 {
-    std::cout << "error: This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout << "<<ERROR>> This is unacceptable! I want to speak to the manager now." << std::endl;
 };
