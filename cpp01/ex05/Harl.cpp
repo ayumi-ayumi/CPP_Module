2d3558c0b2947cb8646ab181d36cfd2328ce93a1
Ayumi <ayuayuayuayus@gmail.com>
2026-07-08 19:26:03 +0200

    DONE ex05

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,56 @@
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+    fs[0] = &Harl::debug;
+    fs[1] = &Harl::info;
+    fs[2] = &Harl::warning;
+    fs[3] = &Harl::error;
+};
+
+Harl::~Harl(){};
+
+enum levelType 
+{
+    debug = 0,
+    info,
+    warning,
+    error,
+    unknown,
+};
+
+levelType getlevelType(const std::string level) {
+    if (level == "DEBUG") return debug;
+    if (level == "INFO")   return info;
+    if (level == "WARNING")  return warning;
+    if (level == "ERROR") return error;
+    return unknown;
+};
+
+void Harl::complain(std::string level)
+{
+    int levelType = getlevelType(level);
+    if (levelType >= 4)
+    {
+        std::cout << "|--- Invalid input ---|" << std::endl;
+        return ;
+    }
+    (this->*fs[levelType])();
+};
+
+void Harl::debug(void)
+{
+    std::cout << "debug: I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+};
+void Harl::info(void)
+{
+    std::cout << "info: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+};
+void Harl::warning(void)
+{
+    std::cout << "warning: I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+};
+void Harl::error(void)
+{
+    std::cout << "error: This is unacceptable! I want to speak to the manager now." << std::endl;
+};
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,30 @@
+#include "Harl.hpp"
+
+int main()
+{
+    std::cout << "Select a level" << std::endl;
+    std::cout << "1. DEBUG" << std::endl;
+    std::cout << "2. INFO" << std::endl;
+    std::cout << "3. WARNING" << std::endl;
+    std::cout << "4. ERROR" << std::endl;
+    int input;
+    std::cin >> input;
+    std::string level;
+    switch (input)
+    {
+        case 1:
+            level = "DEBUG";
+            break;
+        case 2:
+            level = "INFO";
+            break;
+        case 3:
+            level = "WARNING";
+            break;
+        case 4:
+            level = "ERROR";
+            break;
+    }
+    Harl harl;
+    harl.complain(level);
+};
