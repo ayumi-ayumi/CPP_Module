da800d7c5b637615a859a968fc2b380e9494c2d0
Ayumi <ayuayuayuayus@gmail.com>
2026-07-08 20:22:41 +0200

    ADD displaying lines

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -7,11 +7,13 @@ int main()
     std::string *stringPTR = &str;
     std::string &stringREF = str;
 
+    std::cout << "<<Address>>" << std::endl;
     std::cout << "The memory address of the string variable: " << &str << std::endl;
     std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
     std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
     std::cout << std::endl;
 
+    std::cout << "<<Value>>" << std::endl;
     std::cout << "The value of the string variable=> " << str << std::endl;
     std::cout << "The value pointed to by stringPTR=> " << *stringPTR << std::endl;
     std::cout << "The value pointed to by stringREF=> " << stringREF << std::endl;
