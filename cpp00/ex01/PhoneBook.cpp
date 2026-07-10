06a6fd803281f96f7012f3f1f98e834d3b550a59
Ayumi Sato <asato@c4b1c1.42berlin.de>
2026-07-10 19:22:26 +0200

    CHANGE intPhoneBook::execCommand(std::string cmd) to return a value

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -1,45 +1,47 @@
 #include "PhoneBook.hpp"
 #include "Contact.hpp"
 #include <iostream>
+#include <cstdlib>
 
 PhoneBook::PhoneBook()
 {
-    fs[0] = &PhoneBook::add_contact;
-    fs[1] = &PhoneBook::search_contacts;
-    fs[2] = &PhoneBook::exit_contacts;
-};
+	fs[0] = &PhoneBook::add_contact;
+	fs[1] = &PhoneBook::search_contacts;
+	fs[2] = &PhoneBook::exit_contacts;
+}
 
 PhoneBook::~PhoneBook() {};
 
-void    PhoneBook::run()
+void	PhoneBook::run()
 {
-    std::string input;
+	std::string	input;
 	while (1)
 	{
 		std::cout << "\n--------- PHONEBOOK ---------\n";
-        std::cout << "Commands: ADD | SEARCH | EXIT\n";
-        std::cout << "> " << std::flush;
-    	std::getline(std::cin, input);
-		execCommand(input);
+		std::cout << "Commands: ADD | SEARCH | EXIT\n";
+		std::cout << "> " << std::flush;
+		std::getline(std::cin, input);
+		if (!execCommand(input))
+			break;
 	}
 }
 
-void PhoneBook::execCommand(std::string cmd)
+int	PhoneBook::execCommand(std::string cmd)
 {
-    if (cmd == "ADD")
-        return (this->*fs[0])();
-    if (cmd == "SEARCH")
-        return (this->*fs[1])();
-    if (cmd == "EXIT")
-        return (this->*fs[2])();
-    else
-    {
-        std::cout << "|--- Invalid input ---|" << std::endl;
-        return ;
-    }
+	if (cmd == "ADD")
+		return ((this->*fs[0])(), 1);
+	if (cmd == "SEARCH")
+		return ((this->*fs[1])(), 1);
+	if (cmd == "EXIT")
+		return ((this->*fs[2])(), 0);
+	else
+	{
+		std::cout << "|--- Invalid input ---|" << std::endl;
+		return (1);
+	}
 }
 
-void PhoneBook::exit_contacts(void)
+void	PhoneBook::exit_contacts(void)
 {
-     exit(1);
+	return ;
 }
