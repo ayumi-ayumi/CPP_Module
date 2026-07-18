87444fbb8e3347b3290995c5fdd17a1192e7ad72
Ayumi Sato <asato@c1b1c1.42berlin.de>
2026-07-18 17:39:29 +0200

    FORMAT

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -1,57 +1,58 @@
 #include "Harl.hpp"
+#include <iostream>
 
 Harl::Harl()
 {
-    fs[0] = &Harl::debug;
-    fs[1] = &Harl::info;
-    fs[2] = &Harl::warning;
-    fs[3] = &Harl::error;
+	fs[0] = &Harl::debug;
+	fs[1] = &Harl::info;
+	fs[2] = &Harl::warning;
+	fs[3] = &Harl::error;
 };
 
 Harl::~Harl() {};
 
 static int getIdx(std::string level)
 {
-    std::string levelType[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    int idx = -1;
-    for (int i = 0; i < 4; i++)
-    {
-        if (levelType[i] == level)
-        {
-            idx = i;
-            break;
-        }
-    }
-    return (idx);
+	std::string levelType[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int idx = -1;
+	for (int i = 0; i < 4; i++)
+	{
+		if (levelType[i] == level)
+		{
+			idx = i;
+			break;
+		}
+	}
+	return (idx);
 };
 
 void Harl::complain(std::string level)
 {
-    int idx = getIdx(level);
-    if (idx < 0 || idx >= 4)
-    {
-        std::cout << "|--- Invalid input ---|" << std::endl;
-        return;
-    }
-    (this->*fs[idx])();
+	int idx = getIdx(level);
+	if (idx < 0 || idx >= 4)
+	{
+		std::cout << "|--- Invalid input ---|" << std::endl;
+		return;
+	}
+	(this->*fs[idx])();
 };
 
 void Harl::debug(void)
 {
-    std::cout << "<<DEBUG>> I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "<<DEBUG>> I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 };
 
 void Harl::info(void)
 {
-    std::cout << "<<INFO>> I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "<<INFO>> I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 };
 
 void Harl::warning(void)
 {
-    std::cout << "<<WARNING>> I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+	std::cout << "<<WARNING>> I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
 };
 
 void Harl::error(void)
 {
-    std::cout << "<<ERROR>> This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "<<ERROR>> This is unacceptable! I want to speak to the manager now." << std::endl;
 };
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -1,30 +1,31 @@
 #include "Harl.hpp"
+#include <iostream>
 
 int main()
 {
-    std::cout << "Select a level" << std::endl;
-    std::cout << "1. DEBUG" << std::endl;
-    std::cout << "2. INFO" << std::endl;
-    std::cout << "3. WARNING" << std::endl;
-    std::cout << "4. ERROR" << std::endl;
-    int input;
-    std::cin >> input;
-    std::string level;
-    switch (input)
-    {
-        case 1:
-            level = "DEBUG";
-            break;
-        case 2:
-            level = "INFO";
-            break;
-        case 3:
-            level = "WARNING";
-            break;
-        case 4:
-            level = "ERROR";
-            break;
-    }
-    Harl harl;
-    harl.complain(level);
+	std::cout << "Select a level" << std::endl;
+	std::cout << "1. DEBUG" << std::endl;
+	std::cout << "2. INFO" << std::endl;
+	std::cout << "3. WARNING" << std::endl;
+	std::cout << "4. ERROR" << std::endl;
+	int input;
+	std::cin >> input;
+	std::string level;
+	switch (input)
+	{
+		case 1:
+			level = "DEBUG";
+			break;
+		case 2:
+			level = "INFO";
+			break;
+		case 3:
+			level = "WARNING";
+			break;
+		case 4:
+			level = "ERROR";
+			break;
+	}
+	Harl harl;
+	harl.complain(level);
 };
