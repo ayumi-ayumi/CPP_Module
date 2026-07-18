246be3555c47f8844e8cf5ab103a593a99fd00ac
Ayumi Sato <asato@c1b1c1.42berlin.de>
2026-07-18 21:39:22 +0200

    FORMAT

--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -10,7 +10,7 @@ class Harl
 		void	complain(std::string level);
 	private:
 		void	(Harl::*fs[4])(void);
-		void	debug(void) ;
+		void	debug(void);
 		void	info(void);
 		void	warning(void);
 		void	error(void);
