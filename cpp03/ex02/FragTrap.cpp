76752065641edd1f3ff81b722b0a36601371566c
Ayumi <ayuayuayuayus@gmail.com>
2026-07-23 15:58:14 +0200

    EDIT message for copy assigment operator

--- cpp03/ex02/FragTrap.cpp
+++ cpp03/ex02/FragTrap.cpp
@@ -4,19 +4,19 @@
 
 FragTrap::FragTrap() : ClapTrap()
 {
-	std::cout << "FragTrap default constructor created" << std::endl;
 	this->_hitPoints = 100;
 	this->_energyPoints = 100;
 	this->_attackDamage = 30;
+	std::cout << "FragTrap default constructor created" << std::endl;
 }
 
 // Parameterized constructor
 FragTrap::FragTrap(std::string name) : ClapTrap(name)
 {
-	std::cout << "FragTrap " << name << " created" << std::endl;
 	this->_hitPoints = 100;
 	this->_energyPoints = 100;
 	this->_attackDamage = 30;
+	std::cout << "FragTrap " << name << " created" << std::endl;
 }
 
 // Copy constructor
@@ -28,7 +28,7 @@ FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other)
 // Copy assignment operator
 FragTrap& FragTrap::operator=(const FragTrap& other)
 {
-	std::cout << "FragTrap a copy assignment operator constructor created" << std::endl;
+	std::cout << "FragTrap copy assignment operator called" << std::endl;
 	if (this != &other) // Check if you do not assign yourself (a = a;)
 		ClapTrap::operator=(other);
 	return (*this);
