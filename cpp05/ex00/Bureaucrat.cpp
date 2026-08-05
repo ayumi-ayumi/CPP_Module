e4cab8d8a8c5e0898826364b82ad39119a7c897a
Ayumi <ayuayuayuayus@gmail.com>
2026-08-05 16:51:04 +0200

    CHANGE a parameter taking a reference

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -3,7 +3,7 @@
 
 Bureaucrat::Bureaucrat() : _name(""), _grade(150) {}
 
-Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name)
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name(name)
 {
 	if (grade < 1)
 		throw (GradeTooHighException());
