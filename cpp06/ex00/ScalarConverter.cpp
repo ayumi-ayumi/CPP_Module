f097e6a95013fddeac9940e68037a727fb1bc829
Ayumi <ayuayuayuayus@gmail.com>
2026-08-16 19:58:32 +0200

    NEED to add overflow number

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <iomanip>
 
 ScalarConverter::ScalarConverter() {}
 
@@ -18,28 +19,138 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
 
 ScalarConverter::~ScalarConverter() {}
 
-void checkInput(const std::string &str, std::string &type)
-{
-	if (str.length() == 3 && str[0] == '\'' && str[2] == '\'')
-		type = "char";
-	if (str == "nan")
-		type = "nan";
-	if (str[str.length() - 1] == 'f')
-		type = "float";
-	if (str[str.length() - 2] == '.')
-		type = "double";
+enum Type{
+	CHAR,
+	INT,
+	FLOAT,
+	DOUBLE,
+	NAN,
+	NANF,
+	INF,
+	INFF,
+	INVALID
+};
 
+bool isInt(const std::string &str)
+{
+	try
+	{
+		stoi(str);
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+	}
+	
+	int i = 0;
+	if (str[0] == '-' || str[0] == '+')
+		i++;
+	while (str[i])
+	{
+		if (isdigit(str[i]))
+			i++;
+		else
+			return (false);
+	}
+	return (true);
+}
+
+void checkInput(const std::string &str, int &type, int &isNegative)
+{
+	if (str[0] == '-')
+		isNegative = 1;
+	if (str.length() == 3 && str[0] == '\'' && str[2] == '\'')
+		type = CHAR;
+	else if (str == "nan")
+		type = NAN;
+	else if (str == "nanf")
+		type = NANF;
+	else if (str[str.length() - 1] == 'f')
+		type = FLOAT;
+	else if (str[str.length() - 2] == '.')
+		type = DOUBLE;
+	else if (isInt(str))
+		type = INT;
+	else
+		type = INVALID;
 }
 
 void ScalarConverter::convert(const std::string &str)
 {
-	std::string type;
-	checkInput(str, type);
-	std::cout << "type: " << type << std::endl;
-	std::cout << "type outside checkInput: " << &type << std::endl;
-
-	// std::cout << "char: " << static_cast<char>(str) << std::endl;
-	// std::cout << "int: " << static_cast<int>(stoi(str)) << std::endl;
-	// std::cout << "float: " << static_cast<float>(stof(str)) << std::endl;
-	// std::cout << "double: " << static_cast<double>(stod(str)) << std::endl;
+	int type;
+	int isNegative;
+	checkInput(str, type, isNegative);
+	if (type == INVALID)
+	{
+		std::cout << "Invalid input, it cannot be detected" << std::endl;
+		return;
+	}
+	if (type == FLOAT)
+	{
+		std::cout << "char: '" << static_cast<char>(stoi(str)) << "'" << std::endl;
+		std::cout << "int: " << static_cast<int>(stoi(str)) << std::endl;
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "float: " << static_cast<float>(stof(str)) << "f" << std::endl;
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "double: " << static_cast<double>(stod(str)) << std::endl;
+	}
+	if (type == DOUBLE)
+	{
+		if (isNegative)
+			std::cout << "char: impossible" << std::endl;
+		else
+			std::cout << "char: '" << static_cast<char>(stoi(str)) << "'" << std::endl;
+		std::cout << "int: " << static_cast<int>(stoi(str)) << std::endl;
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "float: " << static_cast<float>(stof(str)) << "f" << std::endl;
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "double: " << static_cast<double>(stod(str)) << std::endl;
+	}
+	if (type == CHAR)
+	{
+		char c = str[1];
+		std::cout << "char: " << str << std::endl;
+		std::cout << "int: " << static_cast<int>(c)<< std::endl;
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "float: " << static_cast<float>(c) << "f" << std::endl;
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "double: " << static_cast<double>(c) << std::endl;
+	}
+	if (type == INT)
+	{
+		if (isNegative)
+			std::cout << "char: impossible" << std::endl;
+		else
+			std::cout << "char: '" << static_cast<char>(stoi(str)) << "'" << std::endl;
+		std::cout << "int: " << static_cast<int>(stoi(str)) << std::endl;
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "float: " << static_cast<float>(stof(str)) << "f" << std::endl;
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "double: " << static_cast<double>(stod(str)) << std::endl;
+	}
+	if (type == NAN || type == NANF)
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+	}
+	if (type == INF || type == INFF)
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		if (isNegative)
+		{
+			std::cout << "float: -inff" << std::endl;
+			std::cout << "double: -inf" << std::endl;
+		}
+		else
+		{
+			std::cout << "float: inff" << std::endl;
+			std::cout << "double: inf" << std::endl;
+		}
+	}
 }
